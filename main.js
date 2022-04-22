dd50a7aae1a6e6e9d319540bb41c28303c7b7ae1
gautamvi <85584126+gautamvi@users.noreply.github.com>
2022-04-21 19:07:11 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 status = "";
 input = "";
 
@@ -22,4 +23,32 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            if(objects[i].label == input){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("mentioned_object").innerHTML = "Object mentioned found";
+                SpeechSynthesisUtterance("Object mentioned found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("mentioned_object").innerHTML = "Object mentioned found";
+            }
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("##FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
